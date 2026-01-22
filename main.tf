84cc9d7fcbc7fc2daccfafa069a14366ee6ec6a6
luniemma <luniyisiemmanuel@gmail.com>
2026-01-21 22:38:05 -0600

    updated permition

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ backend "azurerm" {
   resource_group_name  = "terraform-state-rg"
   storage_account_name = "tfstateaksproject"
   container_name       = "tfstate"
-  key                  = "aks.tfstate"
+  key                  = "aks/terraform.tfstate"
     use_azuread_auth     = true # ⭐ ADD THIS LINE
   }
 
